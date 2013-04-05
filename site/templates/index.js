1c4c57c36f936a2c2f63c7377ddebe0faf56698f
Noah Apthorpe <apthorpe@princeton.edu>
2013-04-05 00:34:38 -0400

    added minimal functionality for save-to-cart button

--- site/templates/index.js
+++ site/templates/index.js
@@ -8,13 +8,34 @@ $(document).ready(function() {
             function( data ) {
                 /* put the data in the result div */
                 $("#results_div").empty().append( $( data ) );
+                
+                /* make the course's data show up when it is clicked */
                 $(".course").click(function() {
                     var course_id = $(this).attr('id').split('_')[2];
-                    $(".detail_shown").switchClass("detail_shown", "detail");                        
-                    var detail_id = "#detail_num_"+course_id;
-                    $(detail_id).switchClass("detail", "detail_shown");
-                    $(".course_shown").switchClass("course_shown", "course");
-                    $(this).switchClass("course", "course_shown");
+                    if ($("#detail_num_"+course_id).attr('class') != "detail_shown") {
+                        $(".detail_shown").switchClass("detail_shown", "detail");                        
+                        var detail_id = "#detail_num_"+course_id;
+                        $(detail_id).switchClass("detail", "detail_shown");
+                        $(".course_shown").switchClass("course_shown", "course");
+                        $(this).switchClass("course", "course_shown");
+                    }
+                });
+                
+                /* set the behavior of the "save course to cart button" */
+                $(".savecourse_button").click(function() {
+                    var course_id = this.id.split('_')[1];
+                    
+                    /* add the course to the cart if not already there */
+                    if ($("#cart_num_"+course_id).length == 0) {
+                        $("#cart_list").append(
+                            "<li class='coursecart' id=cart_num_"+course_id+">"+
+                            $("#result_num_"+course_id).clone().children().remove().end().text().split(':')[0]+"</li>"
+                        );
+                       
+                        /* add code to send post request to save that the user has added this course to their cart */
+                        
+                        /* add code to redisplay course information when user selects it in his/her cart */
+                    } 
                 });
             });
     });
