318e48e24903d9a8852a43b3b18db45fadd0b550
Luke Paulsen <luke.paulsen@gmail.com>
2013-04-21 16:15:25 -0400

    setting up to return course reviews on course page. Note that semester menu is now broken since we are switching to request.POST

--- site/djangosite/views.py
+++ site/djangosite/views.py
@@ -8,6 +8,7 @@ from collections import OrderedDict
 
 DISTRIBUTION_AREAS = ['EM', 'EC', 'HA', 'LA', 'QR', 'SA', 'STN', 'STL']
 SUBJECT_AREAS = ["AAS", "AFS", "AMS", "ANT", "AOS", "APC", "ARA", "ARC", "ART", "AST", "ATL", "BCS", "CBE", "CEE", "CHI", "CHM", "CHV", "CLA", "CLG", "COM", "COS", "CWR", "CZE", "DAN", "EAP", "EAS", "ECO", "ECS", "EEB", "EGR", "ELE", "ENE", "ENG", "ENV", "EPS", "FIN", "FRE", "FRS", "GEO", "GER", "GHP", "GLS", "GSS", "HEB", "HIN", "HIS", "HLS", "HOS", "HUM", "ISC", "ITA", "JDS", "JPN", "JRN", "KOR", "LAO", "LAS", "LAT", "LIN", "MAE", "MAT", "MED", "MOD", "MOG", "MOL", "MSE", "MUS", "NES", "NEU", "ORF", "PAW", "PER", "PHI", "PHY", "PLS", "POL", "POP", "POR", "PSY", "QCB", "REL", "RUS", "SAS", "SLA", "SOC", "SPA", "STC", "SWA", "THR", "TPP", "TRA", "TUR", "URB", "URD", "VIS", "WRI", "WWS"]
+DECAY_FACTOR = 0.5
 
 def home(request):
     return render(request, "hello_world.html")
@@ -59,13 +60,19 @@ def search_results(request):
 # Get a new semester and pass it back to the search page.
 def get_semester(request):
     db = CatDB()
-    course = db.get_course({"course_id": request.GET['course_id']})[0]
+    course = db.get_course({"course_id": request.POST['course_id']})[0]
     result = db.get_course({"subject": course['subject'], 
         "course_number": course['course_number'], 
-        "term": request.GET['semester']})[0]
+        "term": request.POST['semester']})[0]
     result = annotate(db, result)
     return render(request, "get_semester.html", {'result': result})
 
+# Get a course's reviews and pass them back.
+def get_reviews(request):
+    db = CatDB()
+    result = db.get_reviews(request.POST['course_id'])
+    return render(request, "get_reviews.html", {'result': result})
+    
 # Add a course to the user's course cart.
 def add_course_cart(request):
     db = CatDB()
@@ -90,10 +97,18 @@ def annotate(db, semester):
         term_no = int(semester['term'])
         semester['term_name'] = term_name(term_no)
     if 'all_terms' in semester:
+        # Add more nice semester names
         all_named_terms = OrderedDict()
         for term in semester['all_terms']:
             all_named_terms[term] = term_name(int(term))
         semester['all_named_terms'] = all_named_terms
+        # Add aggregated review data
+        for term in semester['all_terms']:
+            reviews = db.get_reviews(semester['course_id'])
+            current_weight = 1.0
+            total_weight = 0.0
+            for review in reviews:
+                current_weight = current_weight * DECAY_FACTOR
     return semester
 
 def term_name(term_no):
