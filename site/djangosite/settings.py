a0686bebea91a239c746c130a548fb5093eff435
Luke Paulsen <luke.paulsen@gmail.com>
2013-05-13 22:16:46 -0400

    removed problematic line from settings.py

--- site/djangosite/settings.py
+++ site/djangosite/settings.py
@@ -122,7 +122,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clear_cache',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
